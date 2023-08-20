a4efb056c509908798cbf7503e0ae84701416d32
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-08-20 11:29:02 +0530

    project section completed

--- script.js
+++ script.js
@@ -12,4 +12,22 @@ window.addEventListener("scroll", function() {
   }
 });
 
+let next = document.getElementById("next");
+let prev= document.getElementById("prev")
+let defaultTransform = 0;
+function goNext() {
+    defaultTransform = defaultTransform - 398;
+    var slider = document.getElementById("slider");
+    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
+    slider.style.transform = "translateX(" + defaultTransform + "px)";
+}
+next.addEventListener("click", goNext);
+function goPrev() {
+    var slider = document.getElementById("slider");
+    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
+    else defaultTransform = defaultTransform + 398;
+    slider.style.transform = "translateX(" + defaultTransform + "px)";
+}
+prev.addEventListener("click", goPrev);
+
   
